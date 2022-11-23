66e6f7c9850d9c3c497d05ee379103fddd2cf2b1
Muratova Svetlana <muratova.sa@phystech.edu>
2022-11-23 13:26:35 +0300

    2DZ Form

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,30 @@
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Server server = new Server();
         server.start();
+        server.putHeandler("GET", "/message", ((request, stream) -> {
+            try {
+                stream.write(("GET request \r\n" +
+                        "parametrs: " + request.getQueryParams() + "\r\n").getBytes(StandardCharsets.UTF_8));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
+        server.putHeandler("POST", "/message", new Heandler() {
+            @Override
+            public void handler(Request request, BufferedOutputStream stream) {
+                try {
+                    stream.write((request.getBody() + "\r\n" +
+                            "parametrs: " + request.getQueryParam("name").getValue() + "\r\n").getBytes(StandardCharsets.UTF_8));
+                    System.out.println("POST request");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,17 +1,64 @@
+import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.net.URIBuilder;
+
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.util.List;
+
 public class Request {
 
-    public static String code_200(String mimeType, long length) {
-        return "HTTP/1.1 200 OK\r\n" +
-                "Content-Type: " + mimeType + "\r\n" +
-                "Content-Length: " + length + "\r\n" +
-                "Connection: close\r\n" +
-                "\r\n";
+    private String method;
+    private String pathRequest;
+    private String body;
+    private final URIBuilder uriBuilder;
+
+    public Request(String method, String pathRequest, String body, URIBuilder uriBuilder) throws URISyntaxException {
+        this.method = method;
+        this.body = body;
+        this.pathRequest = pathRequest;
+        this.uriBuilder = new URIBuilder(pathRequest, Charset.defaultCharset());
     }
 
-    public static String code_404() {
-        return "HTTP/1.1 404 Not Found\r\n" +
-                "Content-Length: 0\r\n" +
-                "Connection: close\r\n" +
-                "\r\n";
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getPathRequest() {
+        return pathRequest;
+    }
+
+    public void setPathRequest(String pathRequest) {
+        this.pathRequest = pathRequest;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public URIBuilder getUriBuilder() {
+        return uriBuilder;
+    }
+
+    public NameValuePair getQueryParam(String name) {
+
+        return uriBuilder.getFirstQueryParam(name);
+    }
+
+    public List<NameValuePair> getQueryParams() {
+
+        return uriBuilder.getQueryParams();
+    }
+
+    @Override
+    public String toString() {
+        return "Запрос: [метод " + method + " путь запроса " + pathRequest + " тело запроса " + body + " ]";
     }
 }
